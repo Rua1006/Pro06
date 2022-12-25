4ac9c1138df853891587075fa96f8a69e21df823
Rua1006 <bskang0806@naver.com>
2022-12-25 21:12:36 +0900

    "board detail"

--- myapp/src/main/java/com/shop2/myapp/config/SecurityConfiguration.java
+++ myapp/src/main/java/com/shop2/myapp/config/SecurityConfiguration.java
@@ -13,5 +13,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**").permitAll();
+        http.csrf().disable();
     }
 }
--- myapp/src/main/java/com/shop2/myapp/dto/NoticeDTO.java
+++ myapp/src/main/java/com/shop2/myapp/dto/NoticeDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class NoticeDTO {
+   private int notiSeq;
    private int notiNo;
    private String title;
    private String content;
